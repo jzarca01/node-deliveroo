7d8cec0b2e3dbc9a50d4696a2b7fb2cbb2ceba4b
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-07-06 15:51:28 +0200

    new version

---
+++ README.md
@@ -0,0 +1,58 @@
+# node-deliveroo
+
+An API for Deliveroo
+
+## Usage
+
+```javascript
+const Deliveroo = require('node-deliveroo');
+const deliveroo = new Deliveroo();
+```
+
+### Sign up
+
+```javascript
+deliveroo.signUp(login, password)
+```
+
+### Log in
+
+```javascript
+deliveroo.login(login, password);
+```
+
+### Add a voucher to an account
+
+```javascript
+deliveroo.addVoucherToUser(userId, voucher);
+```
+
+### Get user order history
+
+```javascript
+deliveroo.getHistory(userId)
+```
+
+### Get order details
+
+```javascript
+deliveroo.getOrderDetails(orderId, userId);
+```
+
+### Get available restaurants
+
+```javascript
+deliveroo.getAvailableRestaurants(lat, lng);
+```
+
+### Get available categories
+
+```javascript
+deliveroo.getAvailableCategories(lat, lng);
+```
+
+### Get available restaurant in specific category
+
+```javascript
+deliveroo.getAvailableRestaurantsInCategory(lat, lng, categoryId)
+```
---
+++ index.js
@@ -0,0 +1,166 @@
+const axios = require('axios');
+const faker = require('faker');
+
+class Deliveroo {
+  constructor() {
+    this.request = axios.create({
+      baseURL: 'https://deliveroo.co.uk/orderapp/v1'
+    });
+  }
+
+  setAccessToken(userId, sessionToken) {
+    this.request.defaults.headers.common['Authorization'] = '';
+    delete this.request.defaults.headers.common['Authorization'];
+
+    const accessToken = new Buffer(`${userId}:orderapp_ios,${sessionToken}`).toString('base64')
+    this.request.defaults.headers.common[
+      'Authorization'
+    ] = `Basic ${accessToken}`;
+  }
+
+  async signUp(email, password) {
+    try {
+      let response = await this.request({
+        method: 'POST',
+        url: '/users',
+        data: {
+          password: password,
+          country: "fr",
+          last_name: faker.fake("{{name.lastName}}"),
+          client_type: "orderapp_ios",
+          email: email,
+          marketing_preferences: {
+            generic: "unchecked"
+          },
+          first_name: faker.fake("{{name.firstName}}")
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        responseType: 'json'
+      });
+      this.setAccessToken(response.data.id, response.data.access_token);
+      return response.data;
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+
+  async login(email, password) {
+    try {
+      let response = await this.request({
+        method: 'POST',
+        url: '/login',
+        auth: {
+          username: email,
+          password: password
+        },
+        data: {
+          client_type: "orderapp_ios"
+        },
+        responseType: 'json'
+      });
+      this.setAccessToken(response.data.id, response.data.access_token);
+      return response.data;
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  async addVoucherToUser(userId, voucher) {
+    try {
+      let response = await this.request({
+        method: 'POST',
+        url: `/users/${userId}/vouchers`,
+        data: {
+          redemption_code: voucher
+        },
+        responseType: 'json'
+      });
+      return response.data;
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  async getHistory(userId) {
+    try {
+      let response = await this.request({
+        method: 'GET',
+        url: `/users/${userId}/orders`,
+        data: {
+          client_type: "orderapp_ios"
+        },
+        responseType: 'json'
+      });
+      return response.data;
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  async getOrderDetails(orderId, userId) {
+    try {
+      let response = await this.request({
+        method: 'GET',
+        url: `/users/${userId}/orders/${orderId}`,
+        data: {
+          client_type: "orderapp_ios"
+        },
+        responseType: 'json'
+      });
+      return response.data;
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  async getAvailableRestaurants(lat, lng) {
+    try {
+      let response = await this.request({
+        method: 'GET',
+        url: '/restaurants',
+        params: {
+          lat: lat,
+          lng: lng
+        },
+        data: {
+          client_type: "orderapp_ios"
+        },
+        responseType: 'json'
+      });
+      return response.data.restaurants;
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  async getAvailableCategories(lat, lng) {
+    try {
+      const restaurants = await this.getAvailableRestaurants(lat, lng);
+      return [...restaurants
+          .filter(restaurant => restaurant.menu.menu_tags.length > 0)
+          .map(restaurant => restaurant.menu.menu_tags)
+          .reduce((acc, arr) => [...acc, ...arr], [])
+          .reduce((acc, elem) => acc.set(elem.id, elem), new Map())
+          .values()
+        ]
+        .sort((a, b) => a.id - b.id);
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  async getAvailableRestaurantsInCategory(lat, lng, categoryId) {
+    try {
+      const restaurants = await this.getAvailableRestaurants(lat, lng);
+      return restaurants.filter(restaurant =>
+        restaurant.menu.menu_tags && restaurant.menu.menu_tags.find(category => category.id === parseInt(categoryId))
+      )
+    } catch (error) {
+      console.log('error');
+    }
+  }
+}
+
+module.exports = Deliveroo;
